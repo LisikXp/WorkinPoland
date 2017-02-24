5e5418099492f60594dce650005571b5eb1a471a
LisikXp <lisikx@yandex.ua>
2017-02-24 11:28:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
-//require_once dirname(__FILE__). "/application/views/config/functions.php";
-require_once  '/application/bootstrap.php';
-require_once  '/application/controllers/Controller.php';
-require_once '/application/controllers/View.php';
+//require_once $_SERVER['HTTP_HOST']. "/application/views/config/functions.php";
+require_once  $_SERVER['HTTP_HOST']. '/application/bootstrap.php';
+require_once  $_SERVER['HTTP_HOST']. '/application/controllers/Controller.php';
+require_once  $_SERVER['HTTP_HOST']. '/application/controllers/View.php';
 $app = new Bootstrap();
 echo dirname(__FILE__). "/application/views/anketa.php";
 ?>
