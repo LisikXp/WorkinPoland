d1f9a60621d716e9c45f5059215136bedc5120e5
LisikXp <lisikx@yandex.ua>
2017-02-24 11:10:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
-require_once $_SERVER['DOCUMENT_ROOT']. "/application/views/config/functions.php";
-require_once $_SERVER['DOCUMENT_ROOT']. '/application/bootstrap.php';
-require_once $_SERVER['DOCUMENT_ROOT']. '/application/controllers/Controller.php';
-require_once $_SERVER['DOCUMENT_ROOT']. '/application/controllers/View.php';
-$app = new Bootstrap();
+//require_once $_SERVER['DOCUMENT_ROOT']. "/application/views/config/functions.php";
+//require_once $_SERVER['DOCUMENT_ROOT']. '/application/bootstrap.php';
+//require_once $_SERVER['DOCUMENT_ROOT']. '/application/controllers/Controller.php';
+//require_once $_SERVER['DOCUMENT_ROOT']. '/application/controllers/View.php';
+//$app = new Bootstrap();
 echo $_SERVER['DOCUMENT_ROOT']. "/application/views/anketa.php";
 ?>
