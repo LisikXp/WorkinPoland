c6f5d7d7406c0a82081c3f410c4cc4d5c4c6cfb8
LisikXp <lisikx@yandex.ua>
2017-02-24 13:44:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-//require_once $_SERVER['DOCUMENT_ROOT']. "/application/views/config/functions.php";
+require_once $_SERVER['DOCUMENT_ROOT']. "/application/views/config/functions.php";
 //require_once $_SERVER['DOCUMENT_ROOT']. '/application/bootstrap.php';
 //require_once $_SERVER['DOCUMENT_ROOT']. '/application/controllers/Controller.php';
 //require_once $_SERVER['DOCUMENT_ROOT']. '/application/controllers/View.php';
