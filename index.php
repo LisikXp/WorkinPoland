06b2fd4572053580ab5125dae3f26733853edad2
LisikXp <lisikx@yandex.ua>
2017-02-24 12:07:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
 //require_once  $_SERVER['HTTP_HOST']. '/application/controllers/Controller.php';
 //require_once  $_SERVER['HTTP_HOST']. '/application/controllers/View.php';
 //$app = new Bootstrap();
-echo $_SERVER['REQUEST_URI']. "application/views/anketa.php";
+echo $_SERVER['DOCUMENT_ROOT']. "application/views/anketa.php";
 ?>
